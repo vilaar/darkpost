2af94b876019ea63c7afe9876499d2c05ce99700
vilaar <60707733+vilaar@users.noreply.github.com>
2022-11-04 12:16:42 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const copyToClipboard = () => {
-    document.getElementById('#darkPost-textArea');
+    document.getElementById('darkPost-textArea');
     document.execCommand('copy');
 };
 
@@ -9,11 +9,11 @@ const countWords = (text) => {
         character === " " && numWords ++; 
     });
 
-    document.getElementById('#darkPost-wordCounter').innerHTML = numWords;
+    document.getElementById('darkPost-wordCounter').innerHTML = numWords;
 };
 
 const darkPostAutoSaver = () => {
-    const text = document.getElementById('#darkPost-textArea').value;
+    const text = document.getElementById('darkPost-textArea').value;
     
     localStorage.setItem('darkPostText', text);
     countWords(text);
@@ -21,5 +21,5 @@ const darkPostAutoSaver = () => {
 
 window.onload = (event) => {
     const savedText = localStorage.getItem('darkPostText');
-    document.getElementById('#darkPost-textArea').innerHTML(savedText);
+    document.getElementById('darkPost-textArea').innerHTML(savedText);
 };
