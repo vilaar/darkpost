32751a79440258aeda6e994d9b913632b635d64a
vilaar <60707733+vilaar@users.noreply.github.com>
2022-11-04 11:32:42 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const copyToClipboard = (text) => {
+    document.getElementById(text);
+    document.execCommand('copy');
+};
+
+const countWords = (text) => {
+    let numWords = 0;
+    text.forEach(character => {
+        character === " " && numWords ++; 
+    });
+
+    document.getElementById('#darkPost-wordCounter').innerHTML = numWords;
+};
+
+const darkPostAutoSaver = () => {
+    const text = document.getElementById('#darkPost-textArea').value;
+    
+    localStorage.setItem('darkPostText', text);
+    countWords(text);
+}
+
+window.onload(() => {
+    const savedText = localStorage.getItem('darkPostText');
+    document.getElementById('#darkPost-textArea').innerHTML(savedText);
+});
