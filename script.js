5fedf7b74de046ca442156665eaa3d7ab7ac1134
vilaar <60707733+vilaar@users.noreply.github.com>
2022-11-04 12:49:35 -0500

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const countWords = () => {
     if (text.length > 0) {
         if (numWords === 0) numWords ++;
         
-        text.forEach(character => character === " " && numWords ++; );
+        text.forEach(character => character === " " && numWords ++);
     };
 
     const wordsWord = numWords === 1 ? 'word' : 'words';
